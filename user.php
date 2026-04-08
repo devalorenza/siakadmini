1a7e89548482da9cdd600cb8e0be7ae7da7ba5b6
devalorenza <I43250843@student.polije.ac.id>
2026-04-08 20:28:28 +0700

    Add files via upload

--- user.php
+++ user.php
@@ -1,14 +1,17 @@
 <?php
 abstract class User {
     protected $nama;
+    protected $id;
 
-    public function __construct($nama) {
+    public function __construct($nama, $id) {
         $this->nama = $nama;
+        $this->id = $id;
     }
 
     public function getNama() {
         return $this->nama;
     }
 
-    abstract public function getRole();
+    abstract public function info();
 }
+?>
