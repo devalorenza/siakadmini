fb3c4634799c67f221e085114481a339aff102dc
devalorenza <I43250843@student.polije.ac.id>
2026-04-08 11:27:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SIAKAD MINI</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="header">
+        <h1>🎓 SIAKAD MINI</h1>
+        <p>Sistem Akademik Mahasiswa</p>
+    </div>
+
+    <div class="card" style="text-align:center;">
+
+        <h2>Selamat Datang 👋</h2>
+        <p>Silakan pilih menu di bawah ini</p>
+
+        <a href="form.php">
+            <button>➕ Input Data Mahasiswa</button>
+        </a>
+
+        <a href="data.php">
+            <button>📊 Lihat Data Mahasiswa</button>
+        </a>
+
+    </div>
+
+</div>
+
+</body>
+</html>
