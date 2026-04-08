1f5117f5b38fd9399c46f59955b4c437134047f7
devalorenza <I43250843@student.polije.ac.id>
2026-04-08 20:27:35 +0700

    Add files via upload

---
+++ matakuliah.php
@@ -0,0 +1,11 @@
+<?php
+class MataKuliah {
+    public $nama;
+    public $sks;
+
+    public function __construct($nama, $sks) {
+        $this->nama = $nama;
+        $this->sks = $sks;
+    }
+}
+?>
