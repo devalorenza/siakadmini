ccb7874924637f95e8722f9f80e59756a80b8834
devalorenza <I43250843@student.polije.ac.id>
2026-04-08 20:26:30 +0700

    Add files via upload

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,44 +1,19 @@
 <?php
-require_once 'User.php';
-require_once 'CetakLaporan.php';
-
-class Mahasiswa extends User implements CetakLaporan {
+class Mahasiswa {
+    private $nama;
     private $nim;
-    private $nilai = [];
 
     public function __construct($nama, $nim) {
-        parent::__construct($nama);
+        $this->nama = $nama;
         $this->nim = $nim;
     }
 
-    public function tambahNilai($matkul, $nilai) {
-        $this->nilai[$matkul] = $nilai;
+    public function getNama() {
+        return $this->nama;
     }
 
-    public function hitungIPK() {
-        $total = 0;
-        $jumlah = count($this->nilai);
-
-        foreach ($this->nilai as $n) {
-            $total += $n;
-        }
-
-        return $jumlah > 0 ? $total / $jumlah : 0;
-    }
-
-    public function cetak() {
-        echo "<h3>KHS Mahasiswa</h3>";
-        echo "Nama: " . $this->nama . "<br>";
-        echo "NIM: " . $this->nim . "<br><br>";
-
-        foreach ($this->nilai as $matkul => $nilai) {
-            echo "$matkul : $nilai <br>";
-        }
-
-        echo "<br><b>IPK: " . number_format($this->hitungIPK(), 2) . "</b>";
-    }
-
-    public function getRole() {
-        return "Mahasiswa";
+    public function getNim() {
+        return $this->nim;
     }
 }
+?>
