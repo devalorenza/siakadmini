ffb48acc18b4dcf992d8e5df1264801717433991
devalorenza <I43250843@student.polije.ac.id>
2026-04-08 20:25:28 +0700

    Add files via upload

--- dosen.php
+++ dosen.php
@@ -4,16 +4,13 @@ require_once 'User.php';
 class Dosen extends User {
     private $nidn;
 
-    public function __construct($nama, $nidn) {
-        parent::__construct($nama);
+    public function __construct($nama, $id, $nidn) {
+        parent::__construct($nama, $id);
         $this->nidn = $nidn;
     }
 
-    public function getRole() {
-        return "Dosen";
-    }
-
-    public function infoDosen() {
-        return "Nama: $this->nama | NIDN: $this->nidn";
+    public function info() {
+        return "Dosen: $this->nama (NIDN: $this->nidn)";
     }
 }
+?>
